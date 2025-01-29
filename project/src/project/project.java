2d811f80c9e961171ac2ff76190f408f75bac8eb
prash1046~ <prashant104646@gmail.com~>
2025-01-29 14:59:48 +0530

    day third

--- project/src/project/project.java
+++ project/src/project/project.java
@@ -3,8 +3,27 @@ package project;
 public class project {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// variable declaration-invalid
+		//int for=13;  -keyword should not be as a variable
+		//int num 1=13;  keyword should not take any space
+		int $num1 = 23;
+		int num = 15/2;
+	 float num1 = 15/6;
+		
+    System.out.println($num1);
+    System.out.println(num);
+    System.out.println(num1);
+    int radius=10;
+    double percentage = 3.14*radius*radius;
+    System.out.println(percentage);
+    char ch='a';
+    System.out.println(ch);
+    char ch1=65;
+    System.out.println(ch1);
+    char var1='a';
+    System.out.println(var1);
+    		
+    
 	}
 
 }
